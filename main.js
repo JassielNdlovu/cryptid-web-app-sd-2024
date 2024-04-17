c0e059b21609343a37152996d4070cbd584353fb
00Ducky13 <131754461+00Ducky13@users.noreply.github.com>
2024-04-17 11:06:47 +0200

    Update main.js
    change fill style

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ init();
 function drawHexagon(x, y) {
   ctx.beginPath();
   ctx.globalCompositeOperation = "source-in";
-    ctx.fillStyle = "rgba(0,0,0,0)";
-    ctx.strokeStyle = ctx.fillStyle;
+    ctx.fillStyle = "rgba(0, 0, 155, 0.8)";
   for (var i = 0; i < 6; i++) {
     ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
   }
   ctx.closePath();
   ctx.stroke();
+  ctx.fill();
   ctx.globalCompositeOperation = "source-out";
 }
