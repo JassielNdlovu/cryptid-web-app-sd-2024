d20b17e3bf8428addcd707cefebc663eae0fc709
PalesaMonica <2550752@students.wits.ac.za>
2024-04-19 04:45:42 +0200

    added new files with third oarty authentication

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+
+// Session middleware
+app.use(session({
+    secret: keys.session.cookieKey,
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect to MongoDB
+mongoose.connect(keys.mongodb.dbURI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+// Routes for authentication
+app.use('/auth', authRoutes);
+
+// Serve static files from 'login' directory
+app.use('/login', express.static(path.join(__dirname, 'login')));
+
+// Serve static files from 'images' directory
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Route for serving the homepage HTML file from the 'login' directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login', 'homepage.html'));
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('App now listening for requests on port 3000');
+});
---
+++ routes/auth-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const path = require('path'); // Import the path module to work with file paths
+const passport = require('passport');
+
+// Render login.html
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../login/login.html'));
+});
+
+// Serve style.css
+router.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../login/styles.css')); 
+});
+
+// Serve profile.html
+router.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname, '../login/profile.html')); 
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+    // Handle logout with Passport.js
+    req.logout(() => {
+        res.redirect('/');
+    });
+});
+
+// Google authentication
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.redirect('/auth/profile');
+});
+
+
+module.exports = router;
