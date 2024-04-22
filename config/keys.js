aa727010ccafc3b546f8c7f6bc45b888e0077282
PalesaMonica <2550752@students.wits.ac.za>
2024-04-22 12:59:06 +0200

    modified the files for azure url

--- config/keys.js
+++ config/keys.js
@@ -3,8 +3,8 @@
 
 module.exports = {
     google: {
-        clientID: '1003504533003-t4mp5lstd4iptc8l0vtgjbf646mfp01g.apps.googleusercontent.com',
-        clientSecret: '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'
+        clientID: '1003504533003-n0f6h8orkt10o50pg5vh05g7b5vt32r5.apps.googleusercontent.com',
+        clientSecret: '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'
     },
     mongodb: {
         dbURI: 'mongodb+srv://SDProject:cryptidweb@sdproject.xsfp3sd.mongodb.net/Cryptid?retryWrites=true&w=majority'
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,44 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path'); // Import the path module
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // Make sure to import Strategy
 const keys = require('./keys');
 const User = require('../models/user-model');
 
-passport.serializeUser((user,done)=>{
-   done(null,user.id);
+// Middleware to check if user is authenticated
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+        // Redirect users to Google for authentication
+        return res.redirect('https://cryotid-web-sd.azurewebsites.net/.auth/login/google/callback'); 
+    } 
+    // If user is authenticated, proceed to the next middleware/route handler
+    next();
+};
+
+// Route to render profile.html
+router.get('/', authCheck, (req, res) => {
+    // Sending profile.html file if user is authenticated
+    res.sendFile(path.join(__dirname, '../login/profile.html'));
 });
 
-passport.deserializeUser((id,done)=>{
-    User.findById(id).then((user)=>{
-        done(null,user);
-    });
-    
- });
-
+// Configure passport to use Google OAuth strategy
 passport.use(new GoogleStrategy({
     // OPTIONS FOR STRATEGY
-    callbackURL: '/auth/google/redirect',
+    callbackURL: 'https://cryotid-web-sd.azurewebsites.net/.auth/login/google/callback', // Ensure this matches your configuration in Google Developer Console
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-
 }, (accessToken, refreshToken, profile, done) => {
-
-    //check if user exist 
-    User.findOne({googleId:profile.id}).then((currentUser)=>{
-       if(currentUser){
-            console.log('user is:'+currentUser);
-            done(null,currentUser);
-       }else{
-        //create new user in database
-        new User({
-            username:profile.displayName,
-            googleId:profile.id,
-            thumbnail:profile._json.picture
-        }).save().then((newUser)=>{
-            console.log('new user created:'+ newUser);
-            done(null,newUser);
-        })
-       }
-    })
-    
-   
+    // Check if user exists
+    User.findOne({ googleId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+            console.log('User exists:', currentUser);
+            done(null, currentUser);
+        } else {
+            // Create new user in database
+            new User({
+                username: profile.displayName,
+                googleId: profile.id,
+                thumbnail: profile._json.picture
+            }).save().then((newUser) => {
+                console.log('New user created:', newUser);
+                done(null, newUser);
+            }).catch(err => {
+                console.error('Error creating new user:', err);
+                done(err, null);
+            });
+        }
+    }).catch(err => {
+        console.error('Error finding user:', err);
+        done(err, null);
+    });
 }));
+
+// Serialize and deserialize user
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    }).catch(err => {
+        done(err, null);
+    });
+});
+
+module.exports = router;
