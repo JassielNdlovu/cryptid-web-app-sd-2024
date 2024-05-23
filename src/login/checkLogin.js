c04dd2e36ab458ef9d50f1d4c61fe865669ea196
Jassiel Ndlovu <nkosijassiel@gmail.com>
2024-05-23 22:15:24 +0200

    Updated checkLogin.js for redirection

--- src/login/checkLogin.js
+++ src/login/checkLogin.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch('/.auth/me');
     const userData = await response.json();
 
-    if (userData.clientPrincipal) {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('post_login_redirect_uri')) {
+        window.location.href = params.get('post_login_redirect_uri');
+    }else if (userData.clientPrincipal) {
         window.location.href = "/profile/profile.html";
     }
 });
