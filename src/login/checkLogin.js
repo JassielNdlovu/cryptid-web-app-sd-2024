edf11a53f044de188f9c01ca9c6dcfa1029f9883
Jassiel Ndlovu <nkosijassiel@gmail.com>
2024-05-24 11:29:42 +0200

    Updated checkLogin.js & login.html. This should fix google auth

--- src/login/checkLogin.js
+++ src/login/checkLogin.js
@@ -2,10 +2,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch('/.auth/me');
     const userData = await response.json();
 
-    const params = new URLSearchParams(window.location.search);
-    if (params.get('post_login_redirect_uri')) {
-        window.location.href = params.get('post_login_redirect_uri');
-    }else if (userData.clientPrincipal) {
+    if (userData.clientPrincipal) {
         window.location.href = "/profile/profile.html";
     }
 });
